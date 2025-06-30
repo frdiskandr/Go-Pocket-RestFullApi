491b79bdf4d5ebdddf2f0d8bb43a4f17f013ad6f
farid iskandar <fariditb159@gmail.com>
2025-06-30 13:41:07 +0700

    implement auth token middleware

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,18 +1,29 @@
 package middleware
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/frdiskndr/Go-Pocket-RestFullApi/pkg"
+	"github.com/gofiber/fiber/v2"
+)
 
 type keyLocals interface{}
 var Token keyLocals
 
 func AuthMiddleware(c *fiber.Ctx) error{
-	token := c.Get("Bearier", "")
+	token := c.Get("Authorization", "")
 	if(token == ""){
 		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthorized",
+			"Message": "Unauthorized",
 		})
 	}
 
-	c.Locals(Token, token)
+	validate, err := pkg.VerifyToken(token)
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"Message": "Unauthorized",
+			"Error": err,
+		})
+	}
+
+	c.Locals(Token, validate)
 	return c.Next()
 }
